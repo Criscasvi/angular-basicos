1add437843d82340ee7dc4eed363689bc9fddabd
Cristhian Castillo <criscasvi@gmail.com>
2022-08-28 19:21:46 -0600

    Primera app Angular

---
+++ src/app/dbz/personajes/personajes.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from '@angular/core';
+import {Personaje} from "../interfaces/dbz.interfaces";
+import {DbzService} from "../services/dbz.service";
+
+@Component({
+  selector: 'app-personajes',
+  templateUrl: './personajes.component.html'
+})
+export class PersonajesComponent{
+
+  get personajes() {
+    return this.dbzService.getPersonajes;
+  }
+
+  constructor(private dbzService: DbzService) {}
+
+}
